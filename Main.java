22a150ae67e0a327809e30b15c9d598a38a46bca
Antonio <anviro0501@gmail.com>
2025-03-19 20:03:47 +0100

    Avr_Creado Main

--- Main.java
+++ Main.java
@@ -1,15 +1,78 @@
 package Antonio_Villalba_EntornosDeDesarrollo;
 
-// Instanciamos un cuadrado y un circulo
-Circulo circulo1=new Circulo(3,"verde");Cuadrado cuadrado1=new Cuadrado(3,3,"rojo");
+public class Main {
+    public static void main(String[] args) {
+        // Instanciamos un cuadrado y un circulo
+        Circulo circulo1 = new Circulo(3, "verde");
+        Cuadrado cuadrado1 = new Cuadrado(3, 3, "rojo");
 
-// Calcula area cuadrado LadoA * LadoB
-double ladoA=cuadrado1.getLadoA();double ladoB=cuadrado1.getLadoB();double areal=ladoA*ladoB;System.out.println("Area cuadrado: "+area1);
+        // Calcula area cuadrado usando el método calcularArea
+        double areaCuadrado = cuadrado1.calcularArea();
+        System.out.println("Area cuadrado: " + areaCuadrado);
 
-// Calcula area circulo pi*R2
-double radio1 circulo1.getRadio();double area2=radio1*3.14;System.out.println("Area circulo: "+area2);
+        // Calcula area circulo usando el método calcularArea
+        double areaCirculo = circulo1.calcularArea();
+        System.out.println("Area circulo: " + areaCirculo);
 
-// Diferencia entre areas de cuadrado y circulo
-double diferencia=areal-area2;System.out.println("Diferencia áreas: "+diferencia);
-// Imprimir datos
-System.out.println("Cuadrado: "+cuadrado1);System.out.println("Circulo: "+circulo1);System.out.println("Diferencia: "+diferencia);
+        // Diferencia entre areas de cuadrado y circulo
+        double diferencia = areaCuadrado - areaCirculo;
+        System.out.println("Diferencia áreas: " + diferencia);
+
+        // Imprimir datos
+        System.out.println("Cuadrado: " + cuadrado1);
+        System.out.println("Circulo: " + circulo1);
+        System.out.println("Diferencia: " + diferencia);
+    }
+}
+
+class Cuadrado {
+    private double ladoA;
+    private double ladoB;
+    private String color;
+
+    public Cuadrado(double ladoA, double ladoB, String color) {
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.color = color;
+    }
+
+    public double getLadoA() {
+        return ladoA;
+    }
+
+    public double getLadoB() {
+        return ladoB;
+    }
+
+    public double calcularArea() {
+        return ladoA * ladoB;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuadrado [ladoA=" + ladoA + ", ladoB=" + ladoB + ", color=" + color + "]";
+    }
+}
+
+class Circulo {
+    private double radio;
+    private String color;
+
+    public Circulo(double radio, String color) {
+        this.radio = radio;
+        this.color = color;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public double calcularArea() {
+        return Math.PI * radio * radio;
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo [radio=" + radio + ", color=" + color + "]";
+    }
+}
